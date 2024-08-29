8dfabade7376888dc0e512166ad99fed7445ac62
Sumeya Muhammed <sumenaweya@gmail.com>
2024-08-28 23:29:20 -1100

    delet fetch, add github link

--- static/app.js
+++ static/app.js
@@ -1,23 +1,11 @@
-const form = document.getElementById("searchbar");
+const username = document.querySelector(".entered_username").innerHTML;
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+document.getElementById("github-link").href = `https://github.com/${username}`;
+document.getElementById("github-link").innerText = `@${username}`;
 
-  const username = document.getElementById("Username").value;
-  console.log(`Submitting username: ${username}`);
-
-  fetch("/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "X-Requested-With": "XMLHttpRequest",
-    },
-    body: `username=${encodeURIComponent(username)}`,
-  })
-    .then((response) => response.text())
-    .then((data) => {
-      console.log(`Received data: ${data}`);
-      document.querySelector(".stat-text h2").textContent = data;
-    })
-    .catch((error) => console.error("Error:", error));
+document.getElementById("Username").addEventListener("submit", function () {
+  document.getElementById("github-link").value = "";
+});
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("github-link").value = "";
 });
