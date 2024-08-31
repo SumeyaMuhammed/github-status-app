4b20e75442918f463570f7840ce87f85d960e31b
Sumeya Muhammed <sumenaweya@gmail.com>
2024-08-31 10:40:53 -1100

    form validation

--- stats.py
+++ stats.py
@@ -2,6 +2,14 @@ from flask import session
 import requests
 def display(username):
     if username:
+
+        user_statistics = f"https://github-readme-stats.vercel.app/api?username={username}&theme=react&show_icons=true&hide_border=true&count_private=true"
+        streaks =f"https://streak-stats.demolab.com?user={username}&theme=react&show_icons=true&hide_border=true&count_private=true"
+        languages_used = f"https://github-readme-stats.vercel.app/api/top-langs/?username={username}&theme=react&show_icons=true&hide_border=true&layout=compact"
+        session['user_statistics'] = user_statistics
+        session['streaks'] = streaks
+        session['languages_used'] = languages_used
+
         # Fetch user data
         user_response = requests.get(f'https://api.github.com/users/{username}')
         if user_response.status_code == 200:
@@ -15,9 +23,7 @@ def display(username):
             followers_count = user_data.get('followers', "Error: Followers data not found")
             following_count = user_data.get('following', "Error: Following data not found")
             gists_count = user_data.get('public_gists', "Error: Gists data not found")
-            user_statistics = f"https://github-readme-stats.vercel.app/api?username={username}&theme=react&show_icons=true&hide_border=true&count_private=true"
-            streaks =f"https://streak-stats.demolab.com?user={username}&theme=react&show_icons=true&hide_border=true&count_private=true"
-            languages_used = f"https://github-readme-stats.vercel.app/api/top-langs/?username={username}&theme=react&show_icons=true&hide_border=true&layout=compact"
+            
             # Fetch repository data
             repos_response = requests.get(f'https://api.github.com/users/{username}/repos')
             if repos_response.status_code == 200:
@@ -38,10 +44,32 @@ def display(username):
                 session['user_statistics'] = user_statistics
                 session['streaks'] = streaks
                 session['languages_used'] = languages_used
+        
+
             else:
                 repos_count = "Error: Could not retrieve repositories data"
-            print(user_statistics)
         else:
-            followers_count = "Error: Could not retrieve user data"
+            session['avatar_url'] = "N/A"
+            session['name'] = "N/A"
+            session['login_name'] = "N/A"
+            session['bio'] = "N/A"
+            session['create_date'] = "N/A"
+            session['profile_chart'] = "N/A"
+            session['repos_count'] = "N/A"
+            session['followers_count'] = "N/A"
+            session['following_count'] = "N/A"
+            session['gists_count'] = "N/A"
+
+        
     else:
-        followers_count = following_count = gists_count = repos_count = " "
+        # Clear session data if no username is provided
+        session['avatar_url'] = "N/A"
+        session['name'] = "N/A"
+        session['login_name'] = "N/A"
+        session['bio'] = "N/A"
+        session['create_date'] = "N/A"
+        session['profile_chart'] = "N/A"
+        session['repos_count'] = "N/A"
+        session['followers_count'] = "N/A"
+        session['following_count'] = "N/A"
+        session['gists_count'] = "N/A"
